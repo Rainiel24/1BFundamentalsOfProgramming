4ddfeceddbbd738736d1e0e9dcfe974ecbc0ad88
Rainiel24 <26ur0296@psu.edu.ph>
2026-09-03 05:01:27 +0800

    Add Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,17 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        System.out.println("First Name: Rainiel");
+        // This line prints the First Name
+        System.out.println("Last Name: Ragasa");
+        // This line prints the Last Name
+        System.out.println("Address: San Patricio, Santa Maria, Pangasinan");
+        // This line prints the Address
+        System.out.println("Contact Number: 09694589311");
+        // This line prints the Contact Number
+        System.out.println("Age: 17");
+        // This line prints the Age
+        System.out.println("GWA: 91");
+        // This line prints the GWA
+    }
+}
+// This is the end of the code
